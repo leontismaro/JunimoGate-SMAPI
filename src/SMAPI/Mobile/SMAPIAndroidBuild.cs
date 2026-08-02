ed272dbd5bfc0302d352f929bcc2f5ae252eaadb
leontismaro <268128446+leontismaro@users.noreply.github.com>
2026-08-02 20:20:15 +0800

    build(android): identify mobile runtime batch

--- src/SMAPI/Mobile/SMAPIAndroidBuild.cs
+++ src/SMAPI/Mobile/SMAPIAndroidBuild.cs
@@ -3,5 +3,5 @@ namespace StardewModdingAPI.Mobile;
 
 public static class SMAPIAndroidBuild
 {
-    public const string BuildCode = "junimogate-4.5.2.26-821167e5";
+    public const string BuildCode = "junimogate-4.5.2.27-63982cad";
 }
