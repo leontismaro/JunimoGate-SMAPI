89c922d8960c0c84d7f23a7f43152a6d45eba323
leontismaro <268128446+leontismaro@users.noreply.github.com>
2026-08-02 22:31:17 +0800

    build(android): identify bootstrap cleanup

--- src/SMAPI/Mobile/SMAPIAndroidBuild.cs
+++ src/SMAPI/Mobile/SMAPIAndroidBuild.cs
@@ -3,5 +3,5 @@ namespace StardewModdingAPI.Mobile;
 
 public static class SMAPIAndroidBuild
 {
-    public const string BuildCode = "junimogate-4.5.2.27-63982cad";
+    public const string BuildCode = "junimogate-4.5.2.28-04e59218";
 }
