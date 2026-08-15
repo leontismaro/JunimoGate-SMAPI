0fd4953a9bd17348fa27b49f8d3f93dc00b186a5
leontismaro <268128446+leontismaro@users.noreply.github.com>
2026-08-15 21:51:49 +0800

    build(android): prepare SMAPI 4.5.2.41

--- src/SMAPI/Mobile/SMAPIAndroidBuild.cs
+++ src/SMAPI/Mobile/SMAPIAndroidBuild.cs
@@ -4,6 +4,6 @@ namespace StardewModdingAPI.Mobile;
 public static class SMAPIAndroidBuild
 {
     public const string ApiVersion = "4.5.2";
-    public const string ImplementationVersion = "4.5.2-junimogate.40";
-    public const string BuildCode = "junimogate-4.5.2.40";
+    public const string ImplementationVersion = "4.5.2-junimogate.41";
+    public const string BuildCode = "junimogate-4.5.2.41";
 }
